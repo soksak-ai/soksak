6c298f7a8617a9c7cf63e7b786ded0033e33211c
max <max@blue.tools>
2026-08-21 15:38:29 +0900

    test: reject sibling unit source execution

---
+++ repository_boundary_gate_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+	"testing"
+
+	composition "github.com/soksak/soksak-contract-composition"
+)
+
+func TestRepositoryDoesNotExecuteSiblingUnitSources(t *testing.T) {
+	root, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+	findings, err := composition.CheckRepositoryBoundary(root)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, finding := range findings {
+		t.Errorf("%s:%d %s %s", finding.Path, finding.Line, finding.Rule, finding.Value)
+	}
+}
